aab6970c28236686f8053d3a1c54c9139c7e1a87
cuining <ning.cui@ele.me>
2017-05-11 10:44:41 +0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Page from '../layouts/main'
+import Link from 'next/prefetch'
+import Head from 'next/head'
+
+export default () => (
+  <Page>
+    <Head>
+      <title>cui ning</title>
+    </Head>
+
+    <div className="home">
+      <div className="main">
+        <h1>cui ning</h1>
+        <nav>
+          <a target="_blank" href="https://github.com/cuining">Github</a>
+          <Link href="https://github.com/cuining/blog/issues"><a>Essays</a></Link>
+          <a href="mailto:cncc0824@gmail.com">Email</a>
+        </nav>
+      </div>
+    </div>
+
+    <style jsx>{`
+      .home {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: -1;
+      }
+
+      .main {
+        flex: none;
+        text-align: center;
+      }
+
+      h1 {
+        font-size: 14px;
+        font-weight: normal;
+      }
+
+      nav {
+        margin-top: 20px;
+      }
+
+      a {
+        display: inline-block;
+        margin: 0 15px;
+        text-decoration: none;
+      }
+    `}</style>
+  </Page>
+)
